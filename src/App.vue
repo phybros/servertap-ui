56d9196b10d89fe83e872d78df389b240fd0087a
Will Warren <willdavidwarren@gmail.com>
2021-08-22 16:07:59 -0400

    Adding vuex

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@
       <!--  -->
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="text-h6">ServerTap UI</v-list-item-title>
-          <v-list-item-subtitle>Minecraft server UI</v-list-item-subtitle>
+          <v-list-item-title class="text-h6">ServerTap</v-list-item-title>
+          <v-list-item-subtitle>Server UI</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item link>
+        <v-list-item link :to="{ name: 'Dashboard' }">
           <v-list-item-icon>
             <v-icon>mdi-view-dashboard</v-icon>
           </v-list-item-icon>
@@ -22,7 +22,7 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item link>
+        <v-list-item link :to="{ name: 'Servers' }">
           <v-list-item-icon>
             <v-icon>mdi-server</v-icon>
           </v-list-item-icon>
@@ -37,11 +37,27 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>{{ $route.meta.title }}</v-toolbar-title>
+
+      <v-progress-linear
+        :active="this.$store.state.appLoading"
+        :indeterminate="this.$store.state.appLoading"
+        :hidden="!this.$store.state.appLoading"
+        absolute
+        bottom
+        color="blue"
+      ></v-progress-linear>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="darkMode()">
+        <v-icon>mdi-moon-waxing-crescent</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
       <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -49,5 +65,10 @@
 <script>
 export default {
   data: () => ({ drawer: null }),
+  methods: {
+    darkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
 };
 </script>
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Servers from "../views/Servers.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      title: "Dashboard",
+    },
+  },
+  {
+    path: "/servers",
+    name: "Servers",
+    component: Servers,
+    meta: {
+      title: "Servers",
+    },
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
